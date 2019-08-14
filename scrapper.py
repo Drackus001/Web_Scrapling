2ee6fcd327b8c6be0756b71787b9e285c48334d6
root <root@localhost.localdomain>
2019-08-14 19:50:43 +0530

    sending mail with proper description

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import time
 
 URL = 'https://www.amazon.in/gp/product/B07SQYTHGK/ref=s9_acsd_al_bw_c_x_1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-4&pf_rd_r=2S4FK3XPHMCAD5TEQ65N&pf_rd_t=101&pf_rd_p=0b001d93-3a9d-453e-9bc6-24730b78af61&pf_rd_i=15096150031&th=1'
 
@@ -15,36 +16,39 @@ def check_price():
 
     title = soup.find(id='productTitle').get_text()
     price = soup.find(id='priceblock_ourprice').get_text()
-
-    print("\nProduct_title:", title.strip())
+    title = title.strip()
+    print("\nProduct_title:", title[:23])
 
     price = price.replace(',','')
     price = int(price[2:7])
     print("\nProduct_price:", price)
-    
-    if(price < 57500):
+    fell_price = 57000
+    if(price < fell_price):
         print("\nMail sending...")
-        send_mail()
+        send_mail(title, price)
+    else:
+        print('\nprice is '+str(price)+' which is still more than ',str(fell_price))
     
     
 
-def send_mail():
+def send_mail(title, price):
     """used to send gmail for price drop"""
     #password= "password"
+    #email = "your_mail"
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
     
-    your_email = 'yourmail@gmail.com'
+    your_email = 'your_mail@gmail.com'
     password= "password"
 
-    receipient_email = 'yourmail@gmail.com'
+    receipient_email = 'your_mail@gmail.com'
 
     server.login(your_email,password)
 
-    subject = 'Price fell down'
-    body = 'Chech the amazon link:' + URL
+    subject = 'Price fell down for ' + title[:23]
+    body = 'New Price:'+ str(price) + '\nChech the amazon link:' + URL
 
     msg = f"Subject: {subject}\n\n{body}"
 
@@ -55,6 +59,14 @@ def send_mail():
     )
     print("\nMail send successfully to",receipient_email)
 
-check_price()
+while(True):
+    print("Checking for price drop...")
+    time.sleep(3)
+    check_price()
+    print('waiting for two hour for again checking...')
+    time.sleep(60 * 60 * 2) #checks every two hour
+
+
+
 
 
