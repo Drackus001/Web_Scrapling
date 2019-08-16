c37497517fab2cd0858fbd9d906e65d63b6804ea
root <root@localhost.localdomain>
2019-08-16 10:10:56 +0530

    welcome page is working

--- price_fell_down/price_fell_down/settings.py
+++ price_fell_down/price_fell_down/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'price_fell_down.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
